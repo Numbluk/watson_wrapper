3aba4a8d82aaee94d4db50eab619a5fddd535a37
Lukas <Lukas.Nimmo@gmail.com>
2016-12-02 18:01:06 -0600

    Added ToneAnalyzer namespace that redirects methods to Analyzer

--- tone_analyzer.rb
+++ tone_analyzer.rb
@@ -5,112 +5,119 @@ require 'json'
 require 'pry'
 
 module WatsonWrapper
-  class InvalidContentTypeError < StandardError; end
-  class InvalidTonesError < StandardError; end
-  class ToneAnalyzer
-    include Singleton
-
-    attr_writer :user, :password, :content_type, :sentences, :allow_data_collection
-
-    def tones=(*tones)
-      @tones = tones
+  module ToneAnalyzer
+    def self.method_missing(mthd, *args, &block)
+      Analyzer.public_send(mthd, *args, &block)
     end
 
-    def self.configure(&block)
-      self.instance.configure &block
-    end
+    class InvalidContentTypeError < StandardError; end
+    class InvalidTonesError < StandardError; end
 
-    def configure
-      set_data
-      yield self
-      self
-    end
+    class Analyzer
+      include Singleton
 
-    def get(text)
-      @text = text 
-      uri = URI.parse(build_get_uri)
+      attr_writer :user, :password, :content_type, :sentences, :allow_data_collection
 
-      req = Net::HTTP::Get.new uri
-      if @allow_data_collection
-        req.add_field('X-Watson-Learning-Opt-Out', '1')
-      end
-      req.basic_auth @user, @password
-
-      res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-        http.request(req)
+      def tones=(*tones)
+        @tones = tones
       end
 
-      JSON.parse(res.body)
-    end
-
-    def post(text)
-      @text = text
-      uri = URI.parse(base_uri)
-
-      req = Net::HTTP::Post.new(uri, 'Content-Type': @content_type)
-      req.body = @text
-      req.basic_auth @user, @password
-
-      res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-        http.request(req)
+      def self.configure(&block)
+        self.instance.configure &block
       end
 
+      def configure
+        set_data
+        yield self
+        self
+      end
 
-      JSON.parse(res.body)
-    end
+      def get(text)
+        @text = text 
+        uri = URI.parse(build_get_uri)
 
-    private
+        req = Net::HTTP::Get.new uri
+        if @allow_data_collection
+          req.add_field('X-Watson-Learning-Opt-Out', '1')
+        end
+        req.basic_auth @user, @password
 
-    def set_data
-      @version = 'v3'
-      @version_date = '2016-05-19'
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+          http.request(req)
+        end
 
-      @endpoint = "https://gateway.watsonplatform.net/tone-analyzer/api/#{@version}/tone"
+        JSON.parse(res.body)
+      end
 
-      # TODO: Raise an exception if no valid content type
-      @valid_content_types = ['text/plain', 'text/html', 'application/json']
+      def post(text)
+        @text = text
+        uri = URI.parse(base_uri)
 
-      # Set a @tones_param to filter results on a specific tone, otherwise
-      # results will contain all tones
-      @valid_tones = ['emotion', 'language', 'social']
-      @tones = []
+        req = Net::HTTP::Post.new(uri, 'Content-Type': @content_type)
+        req.body = @text
+        req.basic_auth @user, @password
 
-      # Set to true to remove sentence-level analysis, otherwise results will
-      # contain sentence-level analysis
-      # TODO: Should I raise an exception if @sentences' string form not 'true'
-      # or 'false'?
-      @sentences = false
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+          http.request(req)
+        end
 
-      # Set to true if you do not want Watson to collect your data to improve its
-      # service.
-      @allow_data_collection = false
 
-      # Text must be at least three words long to perform an analysis
-      # TODO: Raise an exception if this is not the case
-      @text = ''
-    end
+        JSON.parse(res.body)
+      end
 
-    def build_get_uri
-      base_uri + '&' + "text=#{text}"
-    end
+      private
 
-    def base_uri
-      @endpoint + '?' +
-        "version=#{@version_date}&" +
-        "tones=#{tones}&" +
-        "sentences=#{sentences}"
-    end
+      def set_data
+        @version = 'v3'
+        @version_date = '2016-05-19'
 
-    def sentences
-      @sentences.to_s
-    end
+        @endpoint = "https://gateway.watsonplatform.net/tone-analyzer/api/#{@version}/tone"
 
-    def tones
-      @tones.join(',')
-    end
+        # TODO: Raise an exception if no valid content type
+        @valid_content_types = ['text/plain', 'text/html', 'application/json']
 
-    def text
-      @text
+        # Set a @tones_param to filter results on a specific tone, otherwise
+        # results will contain all tones
+        @valid_tones = ['emotion', 'language', 'social']
+        @tones = []
+
+        # Set to true to remove sentence-level analysis, otherwise results will
+        # contain sentence-level analysis
+        # TODO: Should I raise an exception if @sentences' string form not 'true'
+        # or 'false'?
+        @sentences = false
+
+        # Set to true if you do not want Watson to collect your data to improve its
+        # service.
+        @allow_data_collection = false
+
+        # Text must be at least three words long to perform an analysis
+        # TODO: Raise an exception if this is not the case
+        @text = ''
+      end
+
+      def build_get_uri
+        base_uri + '&' + "text=#{text}"
+      end
+
+      def base_uri
+        @endpoint + '?' +
+          "version=#{@version_date}&" +
+          "tones=#{tones}&" +
+          "sentences=#{sentences}"
+      end
+
+      def sentences
+        @sentences.to_s
+      end
+
+      def tones
+        @tones.join(',')
+      end
+
+      def text
+        @text
+      end
     end
   end
 end
